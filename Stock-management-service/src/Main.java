fb3a8d0e835f026bfaf19825d840546f442147f1
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-26 13:35:07 +0530

    main method and set variable

--- Stock-management-service/src/Main.java
+++ Stock-management-service/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
+
+    private static Scanner scan = new Scanner(System.in);
+    private static String userName = "k";
+    private static String password = "1";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+ 
+
     }
+
+
+
+   
+
+   
+
+  
+
+
 }
