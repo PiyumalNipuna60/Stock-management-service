bb0a5841838d58af157cf3c07da00d3a03dde9a2
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-26 23:53:15 +0530

    Add content

--- Stock-management-service/src/Main.java
+++ Stock-management-service/src/Main.java
@@ -1,5 +1,5 @@
-import java.util.Scanner;
-import java.util.Stack;
+
+import java.util.*;
 
 public class Main {
 
@@ -7,19 +7,22 @@ public class Main {
     private static String userName = "k";
     private static String password = "1";
 
+
     public static void main(String[] args) {
 
-        loginUser();
-        HomePage();
+//        LoginUser();
+//        HomePage();
+
+        SupplierManage();
 
     }
 
-    private  static void clearConsole() {
-         String os = System.getProperty("os.name");
-        System.out.println(os);
+    private static void clearConsole() {
+        String os = System.getProperty("os.name");
+
         try {
             if (os.equals("Linux")) {
-                System.out.printf("\033\143");
+                System.out.println("\033\143");
             } else if (os.equals("Windows")) {
                 new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             } else {
@@ -32,7 +35,7 @@ public class Main {
         }
     }
 
-    private static void loginUser() {
+    private static void LoginUser() {
 
         /*----------------HEADER START--------------------*/
         System.out.println();
@@ -78,7 +81,7 @@ public class Main {
             }
         } while (currect);
 
-     HomePage();
+        HomePage();
     }
 
     private static void HomePage() {
@@ -103,43 +106,47 @@ public class Main {
         System.out.println("[5] Exit the system");
 
 
-        boolean currect=false;
-       do {
-           System.out.print("\nEnter an option to continue > ");
-           int num = scan.nextInt();
+        boolean currect = false;
+        do {
+            System.out.print("\nEnter an option to continue > ");
+            int num = scan.nextInt();
 
-           switch (num){
-               case 1:
-                   clearConsole();
-                   CredentialsManage();
-                   currect=false;
-                   break;
-               case 2:
-                   clearConsole();
-                   System.out.println("2");
-                   currect=false;
-                   break;
-               case 3:
-                   System.out.println("3");
-                   currect=false;
-                   break;
-               case 4:
-                   System.out.println("4");
-                   currect=false;
-                   break;
-               case 5:
-                   System.out.println("5");
-                   currect=false;
-                   break;
-               default:
-                   System.out.println("Wrong input");
-                   currect=true;
-           }
-       }while (currect);
+            switch (num) {
+                case 1:
+                    clearConsole();
+                    CredentialsManage();
+                    currect = false;
+                    break;
+                case 2:
+                    clearConsole();
+                    SupplierManage();
+                    currect = false;
+                    break;
+                case 3:
+                    clearConsole();
+                    System.out.println("3");
+                    currect = false;
+                    break;
+                case 4:
+                    clearConsole();
+                    LoginUser();
+                    currect = false;
+                    break;
+                case 5:
+                    clearConsole();
+                    ExitSystem();
+                    currect = false;
+                    break;
+                default:
+                    System.out.println("Wrong input");
+                    currect = true;
+            }
+        } while (currect);
 
     }
 
-  
 
 
+ 
+    
 }
