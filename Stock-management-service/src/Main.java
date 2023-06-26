a46989840b7c70cbedd89224d0958760a6b8fe38
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-06-26 13:35:28 +0530

    add clearConsole method

--- Stock-management-service/src/Main.java
+++ Stock-management-service/src/Main.java
@@ -14,7 +14,23 @@ public class Main {
 
     }
 
-
+    private  static void clearConsole() {
+         String os = System.getProperty("os.name");
+        System.out.println(os);
+        try {
+            if (os.equals("Linux")) {
+                System.out.printf("\033\143");
+            } else if (os.equals("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (final Exception e) {
+//handle the exception
+            System.err.println(e.getMessage());
+        }
+    }
 
    
 
